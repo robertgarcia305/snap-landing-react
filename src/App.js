e64619ad4e3666e1bffa4aef126ae8cd0a652f2b
Robert Garcia <robertserranogar@gmail.com>
2023-10-15 08:39:52 -0600

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./styles.css";
+import "./index.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+import Nav from "./Nav";
+import Body from "./Body";
+import Overlay from "./Overlay";
+
+import data from "./bodyData";
+
+const App = () => {
+  const text = data.map((item) => {
+    return <Body item={item} />;
+  });
+
+  // state for nav bar
+  const [isShown, setIsShown] = useState(false);
+  // state for subcollections in nav bar
+  const [navCol, setNavCol] = useState({
+    features: false,
+    company: false
+  });
+
+  // secondary option
+  // const checkNavDisplay = (sub) => {
+  //   if (sub === "company") {
+  //     setNavCol({ ...navCol, [sub]: !navCol.company });
+  //   } else if (sub === "features") {
+  //     setNavCol({ ...navCol, [sub]: !navCol.features });
+  //   }
+  // };
+
+  const checkNavDisplay = (sub) => {
+    setNavCol(() => ({
+      ...navCol,
+      [sub]: !navCol[sub] // Toggle the value
+    }));
+  };
+
+  const toggleNav = () => {
+    setIsShown(!isShown);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav
+        toggle={toggleNav}
+        display={isShown}
+        features={navCol.features}
+        company={navCol.company}
+        checkDisplay={checkNavDisplay}
+      />
+      {text}
+      <Overlay display={isShown} toggle={toggleNav} />
     </div>
   );
-}
+};
 
 export default App;
+
